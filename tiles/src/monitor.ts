046eac1228e0a7847b5d5b580329b13510a71226
Zack Pollard <zackpollard@ymail.com>
2024-08-07 13:08:21 +0100

    wip

--- tiles/src/monitor.ts
+++ tiles/src/monitor.ts
@@ -1,4 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+import { Point } from '@influxdata/influxdb3-client';
+
 type AsyncFn = (...args: any[]) => Promise<any>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,11 +28,14 @@ export function monitorAsyncFunction<T extends AsyncFn>(
   options: Options = { monitorInvocations: true, acceptedErrors: [], thisArg: undefined },
 ): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
   const { monitorInvocations, acceptedErrors } = options;
-
+  const point = Point.measurement(operationName);
+  // for (const [key, value] of Object.entries(defaultTags)) {
+  //   point.setTag(key, value);
+  // }
   return async (...args: Parameters<T>) => {
     if (monitorInvocations) {
       console.log(`${operationName}-invocation`);
-      // await metrics.count(`${operationName}-invocation`)
+      point.setIntegerField('invocation', 1);
     }
     const timer = startTimer();
     try {
@@ -37,15 +43,35 @@ export function monitorAsyncFunction<T extends AsyncFn>(
     } catch (e) {
       if (!acceptedErrors || !acceptedErrors.some((acceptedError) => e instanceof acceptedError)) {
         console.log(`${operationName}-errors`);
-        // await metrics.count(`${operationName}-errors`)
+        point.setIntegerField('errors', 1);
       }
       throw e;
     } finally {
       console.log(`${operationName}-duration`, timer.elapsedMs());
-      // await metrics.durationMilliseconds(
-      //   `${operationName}-duration`,
-      //   timer.elapsedMs(),
-      // )
+      point.setIntegerField('duration', timer.elapsedMs());
+      console.log(point.toLineProtocol()?.toString());
+      await fetch('https://cf-workers.monitoring.immich.cloud/write', {
+        method: 'POST',
+        body: point.toLineProtocol()?.toString(),
+        headers: {
+          'User-Agent': 'InfluxDBClient/1.0',
+          Authorization: `Token YvGUMEDeN5UFk3iJjvGsGMcZNQmNQzlVszYmJkJ2`,
+          Accept: '*/*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow',
+      })
+        .then((response) => {
+          console.log('url', response.url);
+          console.log('status', response.status);
+          console.log('location header', response.headers.get('Location'));
+          for (const [key, value] of response.headers.entries()) {
+            console.log('fetch headers', `${key}: ${value}`);
+          }
+        })
+        .catch((error) => {
+          console.error('error', error);
+        });
     }
   };
 }
