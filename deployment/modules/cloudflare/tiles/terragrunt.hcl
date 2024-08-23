33c138abed21dbc1793d9b9aa854719d28cbc255
Zack Pollard <zackpollard@ymail.com>
2024-08-23 17:42:33 +0100

    ci: deploy tiles gha

--- deployment/modules/cloudflare/tiles/terragrunt.hcl
+++ deployment/modules/cloudflare/tiles/terragrunt.hcl
@@ -3,6 +3,9 @@ terraform {
 
   extra_arguments custom_vars {
     commands = get_terraform_commands_that_need_vars()
+    arguments = [
+      "-var-file=tiles.tfvars.json",
+    ]
   }
 }
 
@@ -12,4 +15,14 @@ include "cloudflare" {
 
 include "root" {
   path = find_in_parent_folders("root.hcl")
+}
+
+locals {
+  tiles_build_dir    = get_env("TILES_BUILD_DIR")
+  vmetrics_api_token = get_env("VMETRICS_API_TOKEN")
+}
+
+inputs = {
+  tiles_build_dir    = local.tiles_build_dir
+  vmetrics_api_token = local.vmetrics_api_token
 }
--- deployment/modules/cloudflare/tiles/workers.tf
+++ deployment/modules/cloudflare/tiles/workers.tf
@@ -2,20 +2,25 @@ data "cloudflare_zone" "immich_cloud" {
   name = "immich.cloud"
 }
 
-resource "cloudflare_worker_script" "tiles" {
+resource "cloudflare_workers_script" "tiles" {
   account_id = var.cloudflare_account_id
   name       = "tiles"
-  content    = file("${path.module}/dist/tiles.js")
+  content    = file("${var.tiles_build_dir}/index.js")
   module     = true
 
   plain_text_binding {
-    name = "CACHE_CONTROL"
-    text = "public, max-age=2678400" // 31 days - cloudflare cdn maximum
+    name = "PMTILES_FILE_NAME"
+    text = var.pmtiles_file_name
   }
 
   plain_text_binding {
-    name = "ALLOWED_ORIGINS"
-    text = "*"
+    name = "ENVINRONMENT"
+    text = "production"
+  }
+
+  secret_text_binding {
+    name = "VMETRICS_API_TOKEN"
+    text = var.vmetrics_api_token
   }
 
   r2_bucket_binding {
@@ -29,14 +34,19 @@ resource "cloudflare_worker_script" "tiles" {
   }
 }
 
-resource "cloudflare_worker_domain" "tiles" {
+resource "cloudflare_workers_domain" "tiles" {
   account_id = var.cloudflare_account_id
   hostname   = "tiles.immich.cloud"
-  service    = cloudflare_worker_script.tiles.id
+  service    = cloudflare_workers_script.tiles.id
   zone_id    = data.cloudflare_zone.immich_cloud.zone_id
 }
 
 import {
-  to = cloudflare_worker_script.tiles
+  to = cloudflare_workers_script.tiles
   id = "${var.cloudflare_account_id}/tiles"
+}
+
+import {
+  to = cloudflare_workers_domain.tiles
+  id = "${var.cloudflare_account_id}/17b3213004b9d5f077563e3c4ea3171ef9d0c5fc"
 }
