14dc3b628d3cc0c37c40df8af52f12099203a823
Zack Pollard <zackpollard@ymail.com>
2024-09-09 15:12:49 +0100

    feat: add R2 buckets for all 5 available cloudflare regions

--- deployment/modules/cloudflare/tiles/r2.tf
+++ deployment/modules/cloudflare/tiles/r2.tf
@@ -1,6 +1,17 @@
+locals {
+  r2_regions = ["WNAM", "ENAM", "WEUR", "EEUR", "APAC"]
+}
+
 resource "cloudflare_r2_bucket" "tiles" {
   account_id = var.cloudflare_account_id
   name       = "tiles"
+}
+
+resource "cloudflare_r2_bucket" "regional_tiles" {
+  for_each   = { for region in local.r2_regions : region => region }
+  account_id = var.cloudflare_account_id
+  name       = "tiles-${lower(each.value)}"
+  location   = each.value
 }
 
 import {
