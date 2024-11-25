a579a9896eb64b98f84dfdc4ca5aa8d24f583bfe
Zack Pollard <zackpollard@ymail.com>
2024-11-25 13:20:39 +0000

    chore: remove old and unused tiles bucket

--- deployment/modules/cloudflare/tiles/r2.tf
+++ deployment/modules/cloudflare/tiles/r2.tf
@@ -2,21 +2,11 @@ locals {
   r2_regions = ["WNAM", "ENAM", "WEUR", "EEUR", "APAC", "OC"]
 }
 
-resource "cloudflare_r2_bucket" "tiles" {
-  account_id = var.cloudflare_account_id
-  name       = "tiles"
-}
-
 resource "cloudflare_r2_bucket" "regional_tiles" {
   for_each   = { for region in local.r2_regions : region => region }
   account_id = var.cloudflare_account_id
   name       = "tiles-${lower(each.value)}"
   location   = each.value
-}
-
-import {
-  to = cloudflare_r2_bucket.tiles
-  id = "${var.cloudflare_account_id}/tiles"
 }
 
 output "r2_regional_buckets" {
