b881f6f7e10c72c8fe4b7e1cd0d0794a528dc2d5
Zack Pollard <zackpollard@ymail.com>
2025-03-26 23:03:20 +0000

    chore: add dark and black theme

--- map-styles/src/themes.ts
+++ map-styles/src/themes.ts
@@ -1,7 +1,7 @@
-import { BLACK, Flavor, LIGHT } from '@protomaps/basemaps';
+import { BLACK, DARK, Flavor, LIGHT } from '@protomaps/basemaps';
 
 export interface ImmichTheme extends Flavor {
-  iconStyle: 'dark' | 'light';
+  iconStyle: 'dark' | 'light' | 'black';
 }
 
 export const IMMICH_LIGHT: ImmichTheme = {
@@ -12,12 +12,18 @@ export const IMMICH_LIGHT: ImmichTheme = {
 
 export const IMMICH_DARK: ImmichTheme = {
   iconStyle: 'dark',
+  ...DARK,
+};
+
+export const IMMICH_BLACK: ImmichTheme = {
+  iconStyle: 'black',
   ...BLACK,
 };
 
 const themes: Record<string, ImmichTheme> = {
   light: IMMICH_LIGHT,
   dark: IMMICH_DARK,
+  black: IMMICH_BLACK,
 };
 
 export default themes;
